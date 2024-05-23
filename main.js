2a415f289189fd5fac1f9e537a440b70c6b71500
Andrey999K <andrey1234xxxx@mail.ru>
2024-05-23 14:22:01 +0300

    feat: add open popup after open page

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ const popupBg = document.querySelector(".popup-bg");
 const popup = document.querySelector(".popup");
 const buttonNotNow = document.querySelector(".popup__button-not-now");
 
+const onPopupOpen = () => {
+  animationOn = true;
+  popupBg.classList.remove("hidden");
+  setTimeout(() => {
+    popupBg.classList.remove("hidden-popup");
+    popup.classList.remove("hide");
+  }, 1);
+  popupOpen = true;
+  setTimeout(() => animationOn = false, 1000);
+};
+
+onPopupOpen();
+
 window.addEventListener("click", event => {
   if (event.target !== buttonNotNow && !event.target.closest(".swiper")) {
     if (!popupOpen && !animationOn) {
-      animationOn = true;
-      popupBg.classList.remove("hidden");
-      setTimeout(() => {
-        popupBg.classList.remove("hidden-popup");
-        popup.classList.remove("hide");
-      }, 1);
-      popupOpen = true;
-      setTimeout(() => animationOn = false, 1000);
+      onPopupOpen();
     }
   }
 });
