86ae51f71548ca87d55d2dd10faf8e5c18560b32
Andrey999K <andrey1234xxxx@mail.ru>
2024-05-23 14:19:37 +0300

    feat: add animation on popup

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 let popupOpen = false;
-const popup = document.querySelector(".popup-bg");
+let animationOn = false;
+const popupBg = document.querySelector(".popup-bg");
+const popup = document.querySelector(".popup");
 const buttonNotNow = document.querySelector(".popup__button-not-now");
 
 window.addEventListener("click", event => {
-  console.log(event.target.closest(".swiper"))
   if (event.target !== buttonNotNow && !event.target.closest(".swiper")) {
-    // console.log(event.target);
-    console.log();
-    console.log(popupOpen, popup);
-    if (!popupOpen) {
-      popup.classList.remove("hidden");
+    if (!popupOpen && !animationOn) {
+      animationOn = true;
+      popupBg.classList.remove("hidden");
+      setTimeout(() => {
+        popupBg.classList.remove("hidden-popup");
+        popup.classList.remove("hide");
+      }, 1);
       popupOpen = true;
+      setTimeout(() => animationOn = false, 1000);
     }
   }
 });
 
 buttonNotNow.addEventListener("click", () => {
-  popup.classList.add("hidden");
-  popupOpen = false;
+  if (!animationOn) {
+    animationOn = true;
+    popupBg.classList.add("hidden-popup");
+    popup.classList.add("hide");
+    popupOpen = false;
+    setTimeout(() => {
+      popupBg.classList.add("hidden");
+      animationOn = false;
+    }, 1000);
+
+  }
 })
